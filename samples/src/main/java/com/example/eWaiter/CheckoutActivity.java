b209c6403aba1f72b19be7c12bc4e0fccd6fbce9
Azat Belgibayev <belg.azat@gmail.com>
2018-04-25 14:18:34 +0300

    hi

---
+++ samples/src/main/java/com/example/eWaiter/CheckoutActivity.java
@@ -0,0 +1,26 @@
+package com.example.eWaiter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class CheckoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_checkout);
+
+
+        TextView tv = findViewById(R.id.choose);
+        tv.setText("Choose the way of payment:\n Cash\n Visa\n MasterCard");
+    }
+
+}
